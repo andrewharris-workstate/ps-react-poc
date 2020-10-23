98b465976672ad3d110cffa8f26b015355e9f267
Austin Everage <austin.everage@workstate.com>
2020-10-23 09:50:33 -0400

    Fix imports for modules
    - Update after moving modules

--- ClientApp/src/App/App.tsx
+++ ClientApp/src/App/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router';
-import { Layout } from '../components/Layout';
-import { Home } from '../components/Home';
+import { Layout } from '../Layout';
+import { Home } from '../Home';
 import { FetchData } from '../components/FetchData';
 import { Counter } from '../components/Counter';
 
