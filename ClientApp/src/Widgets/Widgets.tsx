765cbfd46f3b3b88a58c99c3cb195812adb435af
Austin Everage <austin.everage@workstate.com>
2020-10-23 10:27:58 -0400

    Add widgets module
    - Add view component
    - Add to routes and navigation

---
+++ ClientApp/src/Widgets/Widgets.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Widgets.css";
+import { People } from "./People";
+
+export const Widgets = () => {
+  return (
+    <div className="widgets">
+      <People></People>
+    </div>
+  );
+};
