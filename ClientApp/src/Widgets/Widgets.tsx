b88e2fce6d60664f771deb107694b9039cc06a76
Austin Everage <austin.everage@workstate.com>
2020-10-23 13:12:42 -0400

    Add people component
    - Functionality with placeholder functions
    - Add to Widgets

--- ClientApp/src/Widgets/Widgets.tsx
+++ ClientApp/src/Widgets/Widgets.tsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { People, PeopleFormData } from "./People";
 import "./Widgets.css";
-import { People } from "./People";
 
 export const Widgets = () => {
+  const defaultPeopleForm: PeopleFormData = {
+    name: "",
+    fullTime: false,
+    startDate: new Date().toISOString().substr(0, 10),
+    ssn: "",
+  };
+
+  const [peopleFormData, setPeopleFormData] = useState(defaultPeopleForm);
+  const [peopleWidgetLoading, setPeopleWidgetLoading] = useState(false);
+
+  const onFormChange = (field: string, value: string) => {
+    setPeopleFormData({
+      ...peopleFormData,
+      [field]: value,
+    });
+  };
+
+  const onPeopleSubmit = () => {
+    setPeopleWidgetLoading(true);
+
+    try {
+      // Request to API submission endpoint
+    } catch (e) {
+      // Handle error
+    } finally {
+      setPeopleWidgetLoading(false);
+    }
+  };
+
   return (
     <div className="widgets">
-      <People></People>
+      <People
+        formData={peopleFormData}
+        loading={peopleWidgetLoading}
+        onFormSubmit={onPeopleSubmit}
+        onFormChange={onFormChange}
+      ></People>
     </div>
   );
 };
